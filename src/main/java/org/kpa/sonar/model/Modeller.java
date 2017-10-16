e188f7a77b97d7cacbb2cf925d62759c73ca1410
kruchinin-pavel <kruchinin.1977@ya.ru>
2017-10-16 17:04:56 +0300

    working with modeller

--- src/main/java/org/kpa/sonar/model/Modeller.java
+++ src/main/java/org/kpa/sonar/model/Modeller.java
@@ -1,6 +1,7 @@
 package org.kpa.sonar.model;
 
 import com.jme3.app.SimpleApplication;
+import org.kpa.game.Point3d;
 import org.kpa.sonar.PointCollection;
 import org.kpa.sonar.Surface;
 import org.kpa.sonar.io.XmlTrackReader;
@@ -8,17 +9,27 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Modeller extends SimpleApplication {
     private static Surface coords;
 
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
         coords = loadTracks();
-//        coords = generateSin(16);
+        coords = generateSin(16);
         new Modeller().start();
     }
 
     public static Surface generateSin(int gridFacetSize) {
+//        List<Point3d> pts = new ArrayList<>();
+//        for (int x = -gridFacetSize / 2; x <= gridFacetSize / 2; x++) {
+//
+//            for (int z = -gridFacetSize / 2; z <= gridFacetSize / 2; z++) {
+//                pts.add(new Point3d(x, -10 + Math.sin(x * z), z));
+//            }
+//        }
+//        return new Surface(pts, -1);
         return Surface.generateGrid(gridFacetSize, (x, z) -> -10 + Math.sin(x * z));
     }
 
